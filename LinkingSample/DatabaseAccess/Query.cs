738af5837520b07941f23c3f77c7511057cedf99
Pavan Baldaniya <pavanbaldaniya@monocept.com>
2023-09-15 10:53:09 +0530

    Adding linking sample to demonstrate linking feature for apps targeting .net core and later.

---
+++ LinkingSample/DatabaseAccess/Query.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DatabaseAccess
+{
+    internal class UserList
+    {
+        private static Dictionary<string, string> _users = new Dictionary<string, string> { { "admin", "admin123" } };
+
+        public static bool CheckUserInList(Tuple<string, string> user)
+        {
+            if (_users.ContainsKey(user.Item1) && _users[user.Item1].Equals(user.Item2))
+                return true;
+            else 
+                return false;
+        }
+    }
+
+    public class Query
+    {
+        public bool UserExists(Tuple<string, string> creds)
+        {
+            if (UserList.CheckUserInList(creds))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ LinkingSample/LinkingSample/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LinkingSample.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
